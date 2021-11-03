683cc261f9f0215c88cd184034ada3fed7077bf1
Jack-rm <jackcnsr94@gmail.com>
2021-11-03 16:00:51 +0100

    Initialization model-controller exercise

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('title', 'Home')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h2 class="text-center">Ciao!</h2>
+        </div>
+    </div>
+</div>
+@endsection
